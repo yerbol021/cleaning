c88c35eb02c29c0d95df000d5c858048308c170d
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-10-18 14:11:21 -0700

    Added some structure to the app

---
+++ cleanapp/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="content">
+        <h3>Here is the page</h3>
+      </div>
+    </div>
+  );
+};
+
+export default App;
