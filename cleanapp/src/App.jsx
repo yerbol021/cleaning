989485475907e7c012782a4493e88b152068f052
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-10-18 14:28:04 -0700

    Add navigation component

--- cleanapp/src/App.jsx
+++ cleanapp/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import TopNavBar from "./components/TopNavBar";
 
 const App = () => {
   return (
     <div className="app">
+      <TopNavBar />
       <div className="content">
         <h3>Here is the page</h3>
       </div>
---
+++ cleanapp/src/components/TopNavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TopNavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">future logo //need logo</div>
+      <div className="about">About</div>
+      <div className="services">services</div>
+      <div className="contact">contact</div>
+    </nav>
+  );
+};
+
+export default TopNavBar;
