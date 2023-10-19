a0ea8a041482fe607f577e212bfdbf4fbc8594f5
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-10-18 17:31:47 -0700

    Added the table

--- cleanapp/src/App.jsx
+++ cleanapp/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TopNavBar from "./components/TopNavBar";
+import "./App.css";
 
 const App = () => {
   return (
@@ -7,6 +8,24 @@ const App = () => {
       <TopNavBar />
       <div className="content">
         <h3>Here is the page</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Type1</th>
+              <th>Type2</th>
+              <th>Type3</th>
+              <th>Type4</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>some info1</td>
+              <td>some info2</td>
+              <td>some info3</td>
+              <td>some info4</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
