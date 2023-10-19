5be2aca49c15aa368ab1258088ad592a1be91ff6
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-10-18 22:43:44 -0700

    added logo to Navbar

--- cleanapp/src/components/TopNavBar.jsx
+++ cleanapp/src/components/TopNavBar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "../styles/TopNavBar.scss";
+import logo from "../images/Logo.png";
 
 const TopNavBar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">future logo //need logo</div>
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" />
+      </div>
       <div className="about">About</div>
       <div className="services">services</div>
       <div className="contact">contact</div>
