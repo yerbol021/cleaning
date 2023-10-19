8cf15f4a3b1b9a16d866e3e5ae41d951e69aabf2
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-10-19 13:55:26 -0700

    Made a list for navbar

--- cleanapp/src/components/TopNavBar.jsx
+++ cleanapp/src/components/TopNavBar.jsx
@@ -5,12 +5,24 @@ import logo from "../images/coolday.png";
 const TopNavBar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <img src={logo} alt="Logo" />
+      <a href="/" className="about">
+        <div className="navbar-logo">
+          <img src={logo} alt="Logo" />
+        </div>
+      </a>
+      <div className="navigation-menu">
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
       </div>
-      <div className="about">About</div>
-      <div className="services">Services</div>
-      <div className="contact">Contact us</div>
     </nav>
   );
 };
